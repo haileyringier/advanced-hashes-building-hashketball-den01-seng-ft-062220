ccf4752bc735c8b82ea50906d06db2ca8287de2e
Hailey Ringier <haileyringier@outlook.com>
2020-06-15 21:28:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -9,7 +9,8 @@ def game_hash
         {:player_name => "Brook Lopez", :number => "11", :shoe => "17", :points => "17", :rebounds => "19",:assists => "10", :steals => "3", :blocks => "1", :slam_dunks=> "15"},
         {:player_name => "Mason Plumlee", :number => "1", :shoe => "19", :points => "26", :rebounds => "11",:assists => "6", :steals => "3", :blocks => "8", :slam_dunks=> "5"},
         {:player_name => "Jason Terry", :number => "15", :shoe => "19", :points => "2", :rebounds => "2",:assists => "2", :steals => "4", :blocks => "11", :slam_dunks=> "1"}
-    ],
+       ],
+    }
     
     :away => {
       :team_name => "Charlotte Hornets",
@@ -22,6 +23,6 @@ def game_hash
         {:player_name => "Kemba Walker", :number => "33", :shoe => "15", :points => "6", :rebounds => "12", :assists => "12", :steals => "7", :blocks => "5", :slam_dunks=> "12"}
         ]
     }
-  }
+    }
   hash
 end 
